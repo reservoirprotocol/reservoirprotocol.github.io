bf8574c4567f4570fba0423bd80bf6a332bb9823
Diego Ramos <ramosdiego@pm.me>
2022-01-21 14:30:11 -0400

    initial design updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,22 +1,26 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Reservoir Docs',
+  title: 'Reservoir',
   tagline: 'NFT aggregator',
   url: 'https://reservoirprotocol.github.io/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/reservoir.png',
   organizationName: 'UnevenLabs', // Usually your GitHub org/user name.
   projectName: 'reservoir-docs', // Usually your repo name.
 
-  plugins: [require.resolve("@cmfcmf/docusaurus-search-local")],
+  plugins: [
+    require.resolve('@cmfcmf/docusaurus-search-local'),
+    // require.resolve('docusaurus-tailwindcss'),
+    'docusaurus-tailwindcss',
+  ],
 
   presets: [
     [
@@ -35,8 +39,8 @@ const config = {
             'https://github.com/facebook/docusaurus/edit/main/website/blog/',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-          customCss2: require.resolve("./src/css/colors.css"),
+          customCss: require.resolve('./src/css/custom.css'),
+          customCss2: require.resolve('./src/css/colors.css'),
         },
       }),
     ],
@@ -58,17 +62,29 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "Reservoir Docs",
-        // logo: {
-        //   alt: "Reservoir Logo",
-        //   src: "img/logo.svg",
-        // },
+        title: 'Reservoir',
+        logo: {
+          alt: 'Reservoir Logo',
+          src: 'img/reservoir.svg',
+        },
         items: [
           {
-            to: "/docs/intro",
-            label: "Tutorial",
-            position: "left",
-            className: "persistent",
+            to: '/docs/intro',
+            label: 'Tutorial',
+            position: 'left',
+            className: 'persistent',
+          },
+          {
+            to: '/docs/core-components/API/overview',
+            label: 'API',
+            position: 'left',
+            className: 'persistent',
+          },
+          {
+            to: '/docs/core-components/SDK/overview',
+            label: 'SDK',
+            position: 'left',
+            className: 'persistent',
           },
           // {
           //   to: "/sdk/introduction",
@@ -95,14 +111,14 @@ const config = {
           //   className: "persistent",
           // },
           {
-            href: "https://github.com/reservoirprotocol/reservoir-docs",
-            label: "GitHub",
-            position: "right",
-            className: "persistent",
+            href: 'https://github.com/reservoirprotocol/reservoir-docs',
+            label: 'GitHub',
+            position: 'right',
+            className: 'persistent',
           },
           // {
           //   type: "localeDropdown",
-  
+
           //   //// Optional
           //   position: "right",
           //   // Add additional dropdown items at the beginning/end of the dropdown.
@@ -120,45 +136,45 @@ const config = {
         // style: "dark",
         links: [
           {
-            title: "Github",
+            title: 'Developers',
             items: [
               {
-                label: "reservoir-core",
-                href: "https://github.com/reservoirprotocol/reservoir-core",
+                label: 'Docs',
+                href: '#',
               },
               {
-                label: "reservoir-sdk",
-                href: "https://github.com/reservoirprotocol/reservoir-core/tree/main/packages/sdk",
-              },
-              {
-                label: "reservoir-app",
-                href: "https://github.com/reservoirprotocol/app",
+                label: 'Github',
+                href: '#',
               },
             ],
           },
           {
-            title: "Ecosystem",
+            title: 'Built on Reservoir',
             items: [
               {
-                label: "App",
-                href: "https://www.reservoir.app",
+                label: 'Levee',
+                href: '#',
+              },
+              {
+                label: 'Loot Exchange',
+                href: '#',
               },
             ],
           },
           {
-            title: "Community",
+            title: 'Community',
             items: [
               {
-                label: "Discord",
-                href: "#",
+                label: 'Discord',
+                href: '#',
               },
               {
-                label: "Twitter",
-                href: "#",
+                label: 'Twitter',
+                href: '#',
               },
               {
-                label: "Blog",
-                href: "#",
+                label: 'Blog',
+                href: '#',
               },
             ],
           },
@@ -167,32 +183,32 @@ const config = {
       },
       colorMode: {
         // "light" | "dark"
-        defaultMode: "dark",
-  
+        defaultMode: 'dark',
+
         // Hides the switch in the navbar
         // Useful if you want to support a single color mode
         disableSwitch: false,
-  
+
         // Should we use the prefers-color-scheme media-query,
         // using user system preferences, instead of the hardcoded defaultMode
         respectPrefersColorScheme: true,
-  
+
         // Dark/light switch icon options
         switchConfig: {
           // Icon for the switch while in dark mode
-          darkIcon: "\u{263D}",
-  
+          darkIcon: '\u{263D}',
+
           // Unicode icons such as '\u2600' will work
           // Unicode with 5 chars require brackets: '\u{1F602}'
-          lightIcon: "\u{263C}",
+          lightIcon: '\u{263C}',
         },
       },
       prism: {
-        additionalLanguages: ["solidity"],
+        additionalLanguages: ['solidity'],
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
     }),
-};
+}
 
-module.exports = config;
+module.exports = config
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,114 @@
-import React from 'react'
+import React, { FC } from 'react'
 import Layout from '@theme/Layout'
-import HomepageFeatures from '../components/HomepageFeatures'
+import Head from '@docusaurus/Head'
 
-export default function Home(): JSX.Element {
+const Home: FC = () => {
   return (
     <Layout title="Reservoir Docs" description="Reservoir Documentation">
-      <main style={{ paddingTop: '30px', paddingBottom: '30px' }}>
-        <HomepageFeatures />
+      <Head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </Head>
+      <main className="container">
+        <h2 className="font-bold text-center text-6xl mt-16 mb-12">
+          A Global Pool of NFT Liquidity
+        </h2>
+        <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-20">
+          {[
+            {
+              name: 'Composable',
+              description:
+                'Quickly build marketplaces with aggregated liquidity and custom fees.',
+              url: {
+                title: 'Read this guide',
+                href: '#',
+              },
+              icon: '',
+            },
+            {
+              name: 'Liquid',
+              description:
+                'Tap into instant sell liquidity powered by collection and trait-wide bids.',
+              url: {
+                title: 'Try it out',
+                href: '#',
+              },
+              icon: '',
+            },
+            {
+              name: 'Scalable',
+              description:
+                'Rerum nam aliquam itaque iusto. Ut officia quia tenetu',
+              url: {
+                title: 'View docs',
+                href: '#',
+              },
+              icon: '',
+            },
+            {
+              name: 'Open',
+              description:
+                'Rerum nam aliquam itaque iusto. Ut officia quia tenetu',
+              url: {
+                title: 'How it works',
+                href: '#',
+              },
+              icon: '',
+            },
+          ].map(({ name, description, url }) => (
+            <div
+              key={name}
+              className="flex flex-col px-6 py-5 border border-neutral-600 rounded-lg bg-neutral-900 "
+            >
+              <div className="text-lg font-semibold bg-trueGray-300 mb-2">
+                {name}
+              </div>
+              <div className="mb-3 flex-grow">{description}</div>
+              <a href={url.href} className="text-violet-500 text-lg">
+                {url.title}
+              </a>
+            </div>
+          ))}
+        </div>
+        <h2 className="font-bold text-center text-xl mb-6">
+          Built on Reservoir
+        </h2>
+        <div className="grid gap-5 grid-cols-4 mb-20">
+          {[
+            {
+              name: 'Leeve',
+              description:
+                'Rerum nam aliquam itaque iusto. Ut officia quia tenetu',
+            },
+            {
+              name: 'Instant Capital',
+              description:
+                'Rerum nam aliquam itaque iusto. Ut officia quia tenetu',
+            },
+            {
+              name: 'ENS Cafe',
+              description:
+                'Rerum nam aliquam itaque iusto. Ut officia quia tenetu',
+            },
+            {
+              name: 'Hack The Floor',
+              description:
+                'Rerum nam aliquam itaque iusto. Ut officia quia tenetu',
+            },
+          ].map(({ name, description }) => (
+            <div
+              key={name}
+              className="px-6 py-5 border border-neutral-600 rounded-lg bg-neutral-900 "
+            >
+              <div className="text-lg font-semibold bg-trueGray-300">
+                {name}
+              </div>
+              <div>{description}</div>
+            </div>
+          ))}
+        </div>
       </main>
     </Layout>
   )
 }
+
+export default Home
