1bef4eb702dcadc27711262d064826141050541c
Diego Ramos <ramosdiego@pm.me>
2022-01-25 15:53:54 -0400

    fixed fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,16 +15,6 @@ const Home: FC = () => {
           content="Reservoir is a blockchain protocol and framework making it easier to build NFT marketplaces and tools, sharing a global pool of NFT liquidity."
         />
         <meta name="keywords" content="NFT, API, Protocol" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Montserrat:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <div className={styles.bgImage}>
         <main className={styles.container}>
